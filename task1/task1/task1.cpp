420d1ec3d0bbbadc9744de7eb8ae019d534c21b3
Gosh20 <79201684+Gosh20@users.noreply.github.com>
2021-02-17 14:54:54 +0300

    Add files via upload

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,23 @@
+﻿/// Task1
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+void main()
+{
+    int x;
+    float f;
+    x = 5;
+    f = pow(cos(x), 2) - pow(sin(x), 2);
+    cout.precision(4);
+    cout << f;
+
+    cin >> x;
+    f = pow(cos(x), 2) - pow(sin(x), 2);
+    cout << f;
+
+}
