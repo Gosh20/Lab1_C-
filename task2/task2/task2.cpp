38237a553ac398fd8ae6550de40d46104907e98a
Gosh20 <79201684+Gosh20@users.noreply.github.com>
2021-02-17 14:55:41 +0300

    Add files via upload

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,25 @@
+﻿//Задание 2
+
+#include <iostream>
+
+using namespace std;
+
+double f(double x)
+{
+    double res;
+    res = pow(cos(x), 2) - pow(sin(x), 2);
+    cout.precision(4);
+    return res;
+}
+
+void main()
+{
+    double x;
+
+    cout << "Input x\n";
+    cin >> x;
+
+    double res = f(x);
+    cout << res;
+}
+
---
+++ task3/task3/task3.cpp
@@ -0,0 +1,26 @@
+﻿// Задание 3
+
+#include <iostream>
+
+using namespace std;
+
+double f(double x);
+
+void main()
+{
+    double x;
+
+    cout << "Input x\n";
+    cin >> x;
+
+    double res = f(x);
+    cout << res;
+}
+
+double f(double x)
+{
+    double res;
+    res = pow(cos(x), 2) - pow(sin(x), 2);
+    cout.precision(4);
+    return res;
+}
